7deea273dbd80119423a6fadf1da5d944b018d04
Willyan <willyannov@hotmail.com>
2022-07-20 15:20:47 -0300

    ajustando a home para os padroes do cypress.json

--- cypress/integration/home.spec.js
+++ cypress/integration/home.spec.js
@@ -1,7 +1,7 @@
 describe('Ser um entregador', () => {
 
     it('Ser um entregador Burger Eats', () => {
-        cy.visit('https://buger-eats-qa.vercel.app');
+        cy.visit('/');
         cy.get('a[href="/deliver"]').click();
         cy.get('#page-deliver form h1').should('have.text', 'Cadastre-se para  fazer entregas');
         
